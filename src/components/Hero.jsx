f8ca0827d495d26023cfe82f8019eb1c9fefe207
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-17 18:05:24 -0400

    Add custom class for alignment.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
-import heroImg from "../assets/hero.svg";
+import heroImg from "../assets/site-builder.svg";
 import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 export default function Hero() {
   return (
     <div className="bg-emerald-100 py-24">
-      <div className="mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8">
+      <div className="align-element grid md:grid-cols-2 items-center gap-8">
         <article>
           <h1 className="text-7xl font-bold tracking-wider">I'm Steven</h1>
           <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">
