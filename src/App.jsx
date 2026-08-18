936a074590439c87adf2efd088cacd0c14f21476
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-17 20:09:04 -0400

    Output Projects component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Hero />
       <Skills />
       <About />
+      <Projects />
     </>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,7 @@
+import ProjectsCard from "./ProjectsCard";
+import { projects } from "../data";
+import SectionTitle from "./SectionTitle";
+
 const Projects = () => {
   return <div>Projects</div>;
 };
